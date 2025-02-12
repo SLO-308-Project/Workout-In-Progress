cac72e795835de33771671bf1a1f3a949ee6a5a2
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-02-11 22:07:14 -0800

    chore: reformat to match prettier standards

--- packages/react-frontend/src/testing.test.js
+++ packages/react-frontend/src/testing.test.js
@@ -1,10 +1,7 @@
-
-test('testing test PASS', () => {
+test("testing test PASS", () => {
     expect(5).toBe(5);
 });
 
-test('testing test FAIL', () => {
+test("testing test FAIL", () => {
     expect(5).toBe(4);
 });
-
-
