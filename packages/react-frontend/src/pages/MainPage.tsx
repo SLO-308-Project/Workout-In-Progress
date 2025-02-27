179b8f04bfceb0176248f04e23b3d12b86c452e4
David Voronov <davidvoronof@gmail.com>
2025-02-27 02:19:58 -0800

    add: route for current session page

--- packages/react-frontend/src/pages/MainPage.tsx
+++ packages/react-frontend/src/pages/MainPage.tsx
@@ -1,13 +1,13 @@
 import {Routes, Route} from "react-router-dom";
 import MachinePage from "./MachinePage";
-import CurrentPage from "./Current";
+import CurrentSessionPage from "./CurrentSessionPage";
 import SessionPage from "./SessionPage";
 
 const MainPage = () => {
     return (
         <Routes>
             <Route path="/Machine" element={<MachinePage />}></Route>
-            <Route path="/Current" element={<CurrentPage />}></Route>
+            <Route path="/CurrentSessionPage" element={<CurrentSessionPage />}></Route>
             <Route path="/Sessions" element={<SessionPage />}></Route>
         </Routes>
     );
