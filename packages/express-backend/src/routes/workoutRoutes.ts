f26fd5b9e80c65174dd3143858adfbc531c95837
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-03-11 23:03:22 -0700

    chore: added missing semicolons

--- packages/express-backend/src/routes/workoutRoutes.ts
+++ packages/express-backend/src/routes/workoutRoutes.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from "express";
+import {Router, Request, Response} from "express";
 import workoutServices from "../services/workoutServices";
 // import {sessionType} from "../data/session";
 // import {machineType} from "../data/machine";
@@ -6,40 +6,50 @@ import workoutServices from "../services/workoutServices";
 const router = Router();
 
 // Get all workouts for a session and passes it along to the client
-router.get("/:sessionId", (req: Request, res: Response) => {
+router.get("/:sessionId", (req: Request, res: Response) =>
+{
     workoutServices
         .getWorkout(req.params.sessionId)
-        .then((result) => {
+        .then((result) =>
+        {
             return res.status(201).send(result);
         })
-        .catch((err) => {
+        .catch((err) =>
+        {
             console.log(err);
             return res.send(err);
         });
 });
 
 // Adds a workout with a machineid to a sessionid and passes it along to the client
-router.post("/:sessionId", (req: Request, res: Response) => {
+router.post("/:sessionId", (req: Request, res: Response) =>
+{
     workoutServices
         .addWorkout(req.body.machineId, req.params.sessionId)
-        .then((result) => {
+        .then((result) =>
+        {
             return res.status(201).send(result);
-        }).catch((err) => {
+        })
+        .catch((err) =>
+        {
             console.log(err);
             return res.send(err);
         });
 });
 
-router.delete("/:sessionId/:workoutId", (req: Request, res: Response) => {
+router.delete("/:sessionId/:workoutId", (req: Request, res: Response) =>
+{
     workoutServices
         .removeWorkout(req.params.sessionId, req.params.workoutId)
-        .then((result) => {
+        .then((result) =>
+        {
             return res.status(204).send(result);
-        }).catch((err) => {
+        })
+        .catch((err) =>
+        {
             console.log(err);
             return res.send(err);
         });
 });
-
 
 export default router;
