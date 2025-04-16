33d2a380cd48e7ae71d4271beed836a8400ed2c8
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-04-15 18:05:33 -0700

    bug: fixed mock testing preventing line coverage

--- packages/express-backend/tests/services/workoutServices.test.ts
+++ packages/express-backend/tests/services/workoutServices.test.ts
@@ -15,39 +15,7 @@ describe("Workout Services Tests", () =>
     // Build in memory database for tests
     beforeEach(() =>
     {
-        // Session entry needed for most workout functionality
         jest.clearAllMocks();
-        // const dummySession = new sessionModel({
-        //     _id: new Types.ObjectId("65f18f3ac6dc7f8d5a1234ab"),
-        //     date: new Date("2025-03-08T12:45:00.000Z"),
-        //     time: 6000,
-        //     workout: [
-        //         {
-        //             machineId: new Types.ObjectId("6458a28d1f3d7c9a8e1b2c46"),
-        //             sets: [
-        //                 {reps: 12, weight: 200},
-        //                 {reps: 10, weight: 220},
-        //                 {reps: 8, weight: 240},
-        //                 {reps: 6, weight: 260},
-        //             ],
-        //         },
-        //     ],
-        // });
-        // await dummySession.save();
-
-        // // Machine entry needed for adding workout
-        // const dummyMachine = new machineModel({
-        //     _id: new Types.ObjectId("61f12342cf4b93ee8b0b37d4"),
-        //     name: "Treadmill",
-        //     muscle: "Hamstring",
-        //     attributes: [
-        //         {
-        //             name: "calories",
-        //             unit: "cal",
-        //         },
-        //     ],
-        // });
-        // await dummyMachine.save();
     });
 
     // Clean up database entries
@@ -141,15 +109,33 @@ describe("Workout Services Tests", () =>
     });
 
     // These tests exist to hit the catch blocks
-    test("Get workout --- failure (session not found)", async () =>
+    test("Get workout --- failure (session id not found)", async () =>
     {
-        sessionModel.findById = jest.fn().mockRejectedValue(null);
+        sessionModel.findById = jest.fn().mockResolvedValue(null);
+        const sessionId = "6458a28d1f3d7c9a8e1b2c46";
+        const result = await workoutServices.getWorkout(sessionId);
+        expect(result).toBeFalsy();
+    });
+
+    test("Get workout --- failure (session error)", async () =>
+    {
+        sessionModel.findById = jest.fn().mockImplementation(async () =>
+        {
+            await Promise.reject(new Error("boom"));
+        });
+        const sessionId = "6458a28d1f3d7c9a8e1b2c46";
+        const result = await workoutServices.getWorkout(sessionId);
+        expect(result).toBeNull();
+    });
+
+    test("Remove workout --- failure (session not found)", async () =>
+    {
+        const sessionId = "";
+        const workoutId = "6458a28d1f3d7c9a8e1b2c46";
         try
         {
-            // invalid session id forces error
-            const sessionId = "6458a28d1f3d7c9a8e1b2c46";
-            await workoutServices.getWorkout(sessionId);
-            fail("Test shouldn't get here");
+            await workoutServices.removeWorkout(sessionId, workoutId);
+            fail("Test Should Not Be Here");
         }
         catch (error)
         {
@@ -159,34 +145,49 @@ describe("Workout Services Tests", () =>
 
     test("Remove workout --- failure (session not found)", async () =>
     {
-        sessionModel.findOne = jest.fn().mockRejectedValue(null);
-        try
+        sessionModel.findOne = jest.fn().mockResolvedValue(null);
+        const sessionId = "6458a28d1f3d7c9a8e1b2c46";
+        const workoutId = "6458a28d1f3d7c9a8e1b2c46";
+        const result = await workoutServices.removeWorkout(
+            sessionId,
+            workoutId,
+        );
+        expect(result).toBeFalsy();
+    });
+
+    test("Remove workout --- failure (session error)", async () =>
+    {
+        sessionModel.findOne = jest.fn().mockImplementation(async () =>
         {
-            const sessionId = "6458a28d1f3d7c9a8e1b2c46";
-            const workoutId = "6458a28d1f3d7c9a8e1b2c46";
-            await workoutServices.removeWorkout(sessionId, workoutId);
-            fail("Test shouldn't get here");
-        }
-        catch (error)
-        {
-            expect(error).toBeTruthy();
-        }
+            await Promise.reject(new Error("boom"));
+        });
+        const sessionId = "6458a28d1f3d7c9a8e1b2c46";
+        const workoutId = "6458a28d1f3d7c9a8e1b2c46";
+        const result = await workoutServices.removeWorkout(
+            sessionId,
+            workoutId,
+        );
+        expect(result).toBeNull();
     });
 
     test("Add workout --- failure (session id not found)", async () =>
     {
-        sessionModel.findOne = jest.fn().mockRejectedValue(null);
-        try
+        sessionModel.findOne = jest.fn().mockResolvedValue(null);
+        const sessionId = "6458a28d1f3d7c9a8e1b2c46";
+        const machineId = "61f12342cf4b93ee8b0b37d4";
+        const result = await workoutServices.addWorkout(machineId, sessionId);
+        expect(result).toBeFalsy();
+    });
+
+    test("Add workout --- failure (session id not found)", async () =>
+    {
+        sessionModel.findOne = jest.fn().mockImplementation(async () =>
         {
-            // invalid session id forces error
-            const sessionId = "6458a28d1f3d7c9a8e1b2c46";
-            const machineId = "61f12342cf4b93ee8b0b37d4";
-            await workoutServices.addWorkout(machineId, sessionId);
-            fail("Test shouldn't get here");
-        }
-        catch (error)
-        {
-            expect(error).toBeTruthy();
-        }
+            await Promise.reject(new Error("boom"));
+        });
+        const sessionId = "6458a28d1f3d7c9a8e1b2c46";
+        const machineId = "61f12342cf4b93ee8b0b37d4";
+        const result = await workoutServices.addWorkout(machineId, sessionId);
+        expect(result).toBeNull();
     });
 });
