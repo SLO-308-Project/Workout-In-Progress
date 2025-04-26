990fb0ced9160e89ad1e321ec0b5e0b43f3b4335
David Voronov <davidvoronof@gmail.com>
2025-04-26 10:55:38 -0700

    feat: login page with styling

--- packages/native-frontend/app/login.tsx
+++ packages/native-frontend/app/login.tsx
@@ -1,11 +1,13 @@
-import { Text, View } from 'react-native';
+import { Text, View, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
 import LoginPrompt from "@/components/login/loginPrompt";
 import { fetchLogin } from "@/fetchers/authFetchers";
 import { useState } from "react";
+import { Redirect } from "expo-router";
 
 export default function Login() {
-    const [loginFailed, setLoginFailed ]= useState(false);
+    const [loginFailed, setLoginFailed] = useState(false);
+    const [loggedIn, setLoggedIn] = useState(false);
     function handleLogin(email: string, password: string): void {
         console.log(`called handle login: email=${email} password=${password}`);
         fetchLogin(email, password)
@@ -21,22 +23,33 @@ export default function Login() {
             .then((res_data) => {
                 // NOTE: login token is being retrieved, but not being stored anywhere yet for protected routing
                 console.log((`auth token: ${res_data}`));
+                setLoggedIn(true);
             })
             .catch((error: Error) => {
                 console.log(`fetchLogin err: ${error.message}`);
             })
     }
+    if (loggedIn) {
+        return (<Redirect href="/(tabs)" />)
+    }
 
     return (
         <SafeAreaView edges={["top"]} className="flex-1 bg-white px-4 pt-4">
-            <View
-                className="container"
-            >
-                <Text className="text-3xl font-semibold text-black tracking-tight pt-4">
-                    Login
+            <View className="items-center" style={{ marginTop: '20%' }}>
+                <Text className="text-4xl font-semibold text-black tracking-tight">
+                    Workout In Progress
                 </Text>
-                <LoginPrompt handleSubmit={handleLogin} loginFailed={loginFailed}/>
             </View>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === "ios" ? "padding" : "height"}
+                    className="flex-1"
+                >
+                    <View className="flex-1 justify-center items-center">
+                        <LoginPrompt handleSubmit={handleLogin} loginFailed={loginFailed} />
+                    </View>
+                </KeyboardAvoidingView>
+            </TouchableWithoutFeedback>
         </SafeAreaView>
     )
 
