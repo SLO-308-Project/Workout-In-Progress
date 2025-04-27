ad7f6ac785a902d06ae9035ce0156071573e72d9
David Voronov <davidvoronof@gmail.com>
2025-04-26 17:49:15 -0700

    add: toggle switch

--- packages/native-frontend/app/settings.tsx
+++ packages/native-frontend/app/settings.tsx
@@ -1,11 +1,20 @@
-import {View, Text, Pressable, ScrollView} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
+import { View, Text, Pressable, ScrollView, Switch } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
 
-export default function SettingsScreen()
-{
+export default function SettingsScreen() {
+    const [darkMode, setDarkMode] = useState(false);
+    // dark mode toggle
     return (
-        <SafeAreaView>
-            <Text>This is settings.</Text>
+        <SafeAreaView className="flex-1 px-4">
+            <View className="mb-4 w-full flex-row items-center justify-between bg-white p-2 rounded-lg shadow">
+                <Text className="font-medium">Dark Mode</Text>
+                <Switch value={darkMode} onValueChange={setDarkMode} />
+            </View>
+            <View className="mb-4 w-full flex-row items-center justify-between bg-white p-2 rounded-lg shadow">
+                <Text className="font-medium">Dark Mode</Text>
+                <Switch value={darkMode} onValueChange={setDarkMode} />
+            </View>
         </SafeAreaView>
     )
 }
