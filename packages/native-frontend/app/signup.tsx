6eba5ba1eb00e4d3a0a4576fe3a464a3e19932ab
Paul Motter <pmotter@calpoly.edu>
2025-04-28 13:40:47 -0700

    Feat: Incomplete but updates to signup.

--- packages/native-frontend/app/signup.tsx
+++ packages/native-frontend/app/signup.tsx
@@ -1,22 +1,58 @@
-import {View, Text} from "react-native";
+import {View, Text, KeyboardAvoidingView, TouchableWithoutFeedback, Platform, Keyboard} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
 import SignupPrompt from "@/components/signup/signupPrompt";
 import { fetchRegister } from "@/fetchers/userFetchers";
+import { useState } from "react";
 
 export default function signup()
 {
+    const [emailExists, setEmailExists] = useState(false);
+
+    /**
+     * Registers a user.
+     * If user exists then sets emailExists to show message.
+     * IF user doesn't exists then creates the user and logs in the user. 
+     */
     function handleSignUp(userName: string, email: string, password: string)
     {
-        console.log(`called handle signup: userName=${userName}, email=${email}, password=${password}`) 
-        
+        console.log(`called handle signup: userName=${userName}, email=${email}, password=${password}`);
+        fetchRegister(userName, email, password)
+            .then((res: Response) => {
+                if (res.ok)
+                    {
+                       //Implement Login and authorization externally.
+                    }
+                else if (res.status == 409) //User already exists. Checks by unique email.
+                    {
+                        setEmailExists(true);
+                    }
+            })
+            .catch((error: Error) => {
+                console.log(`fetchRegister error: ${error.message}`)
+            });
     }
 
     return (
-        <SafeAreaView>
-            <View>
-                <Text>Sign Up</Text>
+        <SafeAreaView
+            className="flex-1 bg-white px-4 pt-4"    
+            edges={["top"]}
+        >
+            <View
+                className="items-center"
+                style={{ marginTop: "20%" }}
+            >
+                <Text
+                    className="text-4x1 font-semibold text-black tracking-tight"
+                >Sign Up</Text>
             </View>
-            <SignupPrompt handleSignUp={handleSignUp} />
+               <TouchableWithoutFeedback>
+                    <KeyboardAvoidingView
+                        behavior={Platform.OS === "ios" ? "padding" : "height"}
+                        className="flex-1"
+                    >
+                        <SignupPrompt handleSignUp={handleSignUp} emailExists={emailExists} />
+                    </KeyboardAvoidingView>
+                </TouchableWithoutFeedback>
         </SafeAreaView>
     );
 }
--- packages/native-frontend/components/signup/signupPrompt.tsx
+++ packages/native-frontend/components/signup/signupPrompt.tsx
@@ -1,20 +1,24 @@
-import {useState} from "react";
-import {Pressable, Text, TextInput, View} from "react-native";
+import { useState } from "react";
+import { Pressable, Text, TextInput, View } from "react-native";
 
-export default function signupPrompt({handleSignUp}: Props)
-{
+type Props = {
+    handleSignUp: (userName: string, email: string, password: string) => void;
+    emailExists: boolean;
+}
+
+export default function signupPrompt({handleSignUp, emailExists}: Props) {
     const [userName, setUserName] = useState<string>("");
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
     return (
         <View>
-            <TextInput
+            <TextInput 
                 onChangeText={(text) => setUserName(text.trim())}
                 placeholder="userName"
                 placeholderTextColor="7A7674"
                 textAlignVertical="center"
-                style={{lineHeight: 16}}
+                style={{ lineHeight: 16}}
                 autoCapitalize="none"
             />
             <TextInput
@@ -22,21 +26,27 @@ export default function signupPrompt({handleSignUp}: Props)
                 placeholder="Email"
                 placeholderTextColor="7A7674"
                 textAlignVertical="center"
-                style={{lineHeight: 16}}
+                style={{ lineHeight: 16}}
                 autoCapitalize="none"
             />
             <TextInput
-                onChangeText={(text) => setUserName(text.trim())}
+                onChangeText={(text) => setPassword(text)}
                 placeholder="password"
                 placeholderTextColor="7A7674"
                 textAlignVertical="center"
-                style={{lineHeight: 16}}
+                style={{ lineHeight: 16}}
                 autoCapitalize="none"
             />
             <Text>Password must be at least 11 characters</Text>
-            <Pressable onPress={}>
+            {
+                emailExists && 
+                <Text>User already exists with that email</Text>
+            }
+            <Pressable
+                onPress={() => handleSignUp(userName, email, password)}
+            >
                 <Text>Sign Up</Text>
             </Pressable>
         </View>
-    );
+    )
 }
