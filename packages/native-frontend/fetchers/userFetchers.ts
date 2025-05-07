b858c0c95932ff775f58b7e464fae1919142ee24
Paul Motter <pmotter@calpoly.edu>
2025-05-07 11:49:35 -0700

    Test2

--- packages/native-frontend/fetchers/userFetchers.ts
+++ packages/native-frontend/fetchers/userFetchers.ts
@@ -1,5 +1,5 @@
-// import Constants from "expo-constants";
-// const BACKEND_URL = Constants.manifest2?.extra?.BACKEND_URL;
+import Constants from "expo-constants";
+const BACKEND_URL = Constants.expoConfig?.extra?.BACKEND_URL;
 
 /**
  * Calls POST to users. Creating a user in the DB. 
@@ -11,8 +11,8 @@ function fetchRegister(
     password: string | undefined,
 ): Promise<Response>
 {
-    return fetch(`/users/register`, {
-    // return fetch(`${BACKEND_URL}/users/register`, {
+    // return fetch(`/users/register`, {
+    return fetch(`${BACKEND_URL}/users/register`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
