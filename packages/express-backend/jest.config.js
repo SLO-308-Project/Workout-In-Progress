249a340a18ff946dc76f69944ca17e76f1eaba90
Paul Motter <pmotter@calpoly.edu>
2025-05-09 07:57:51 -0700

    Revert "Test Precommit"
    This reverts commit 5c806d02aee758c573ab0ea772e5d721b35ed28a.

--- packages/express-backend/jest.config.js
+++ packages/express-backend/jest.config.js
@@ -1,6 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
     testEnvironment: "node",
-    transform: {        "^.+.tsx?$": ["ts-jest", {}],},
-    testTimeout: 20000,};
-// Test
+    transform: {
+        "^.+.tsx?$": ["ts-jest", {}],
+    },
+    testTimeout: 20000,
+};
