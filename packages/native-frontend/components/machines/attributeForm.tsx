5e59d1285b4d0974c5d93cd2752afa3cfdf26821
David Voronov <davidvoronof@gmail.com>
2025-05-12 20:30:15 -0700

    ok scroll picker?

--- packages/native-frontend/components/machines/attributeForm.tsx
+++ packages/native-frontend/components/machines/attributeForm.tsx
@@ -1,17 +1,16 @@
-import {Unit} from "@/types/unit";
-import {Attribute} from "@/types/attribute";
-import {useState} from "react";
-import {View, TextInput, Pressable, Text, Modal} from "react-native";
-import {Picker} from "@react-native-picker/picker";
+import { Unit } from "@/types/unit";
+import { Attribute } from "@/types/attribute";
+import { useState } from "react";
+import { View, TextInput, Pressable, Text, Modal } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import {BlurView} from "expo-blur";
+import ScrollPicker from "react-native-wheel-scrollview-picker";
 
 type Props = {
     handleAddAttribute: (attribute: Attribute) => void;
 };
 
-function AttributeForm({handleAddAttribute}: Props)
-{
+function AttributeForm({ handleAddAttribute }: Props) {
     const [attribute, setAttribute] = useState<Attribute>({
         name: "",
         unit: Unit.LBS, // default is the first unit in the enum. which is lbs
@@ -22,21 +21,22 @@ function AttributeForm({handleAddAttribute}: Props)
         <Picker.Item key={opt} label={Unit[opt]} value={Unit[opt]} />
     ));
 
-    function handleUnitChange(unit: Unit)
-    {
+    function handleUnitChange(unitIndex: number) {
+        const unit = Object.entries(Unit)[unitIndex][1];
         setAttribute({
             ...attribute,
             unit: unit,
         });
     }
 
-    function handleNameChange(name: string)
-    {
+    function handleNameChange(name: string) {
         setAttribute({
             ...attribute,
             name: name,
         });
     }
+
+    // TODO: The scroll picker here is kind of awful.
 
     return (
         <View className="bg-white border border-gray-200 rounded-lg shadow-sm p-2 m-2 flex-row justify-between items-center">
@@ -47,9 +47,24 @@ function AttributeForm({handleAddAttribute}: Props)
                 placeholderTextColor="#A0A0A0"
                 onChangeText={(name) => handleNameChange(name)}
             />
-            <Pressable className="w-20 bg-gray-100 px-4 py-3 border-gray-200 rounded-xl text-base 900 items-center">
-                <Text className="justify-center">{attribute.unit}</Text>
-            </Pressable>
+            {showPicker &&
+                <ScrollPicker
+                    onTouchEnd={() => setShowPicker(false)}
+                    dataSource={Object.values(Unit)}
+                    wrapperBackground="#FFF"
+                    wrapperHeight={60}
+                    onValueChange={(data, selectedIndex) => handleUnitChange(selectedIndex)}
+
+                />
+            }
+            {!showPicker &&
+                <Pressable
+                    className="w-20 bg-gray-100 px-4 py-3 border-gray-200 rounded-xl text-base 900 items-center"
+                    onPress={() => setShowPicker(true)}
+                >
+                    <Text className="justify-center">{attribute.unit}</Text>
+                </Pressable>
+            }
             <Pressable
                 onPress={() => handleAddAttribute(attribute)}
                 className="p-2 rounded-full active:scale-90"
@@ -57,15 +72,13 @@ function AttributeForm({handleAddAttribute}: Props)
                 <AntDesign name="pluscircle" size={24} color="#34C759" />
             </Pressable>
         </View>
+
     );
 }
-// <View className="w-20 bg-gray-50 px-4 py-3 border border-gray-200 rounded-xl mb-4">
-//     <Picker
-//         selectedValue={attribute.unit}
-//         onValueChange={(value) => handleUnitChange(value)}
-//     >
-//         {listAttributeEnum}
-//     </Picker>
-// </View>
+// <Pressable
+//     className="w-20 bg-gray-100 px-4 py-3 border-gray-200 rounded-xl text-base 900 items-center"
+// >
+//     <Text className="justify-center">{attribute.unit}</Text>
+// </Pressable>
 
 export default AttributeForm;
