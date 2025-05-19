e02bf6498adbfa7b3ba56eec01e769981fe9b656
Paul Motter <pmotter@calpoly.edu>
2025-05-19 15:23:06 -0700

    fix: vertical spacing.

--- packages/native-frontend/app/(tabs)/machines.tsx
+++ packages/native-frontend/app/(tabs)/machines.tsx
@@ -1,77 +1,92 @@
 import "@/global.css";
-import { Text, FlatList, Pressable, View, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { useState, useEffect, useCallback } from "react";
+import {Text, FlatList, Pressable, View, StyleSheet} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {useState, useEffect, useCallback} from "react";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import { SearchBar } from "@rneui/themed";
-import { useRouter, useFocusEffect } from "expo-router";
-import { useIsFocused } from "@react-navigation/native";
+import {SearchBar} from "@rneui/themed";
+import {useRouter, useFocusEffect} from "expo-router";
+import {useIsFocused} from "@react-navigation/native";
 
-import MachineComponent, { Empty } from "@/components/machines/machineComponent";
+import MachineComponent, {Empty} from "@/components/machines/machineComponent";
 import {
     fetchGetMachine,
     fetchPostMachine,
     fetchDeleteMachine,
 } from "@/fetchers/machineFetchers";
 
-import { Machine } from "@/types/machine";
+import {Machine} from "@/types/machine";
 
-function MachinePage() {
+function MachinePage()
+{
     const [machines, setMachine] = useState<Machine[]>([]);
     const [search, setSearch] = useState<string>("");
     const router = useRouter();
     const isFocused = useIsFocused();
 
-    const updateSearch = (search: string) => {
+    const updateSearch = (search: string) =>
+    {
         setSearch(search);
     };
 
-    useEffect(() => {
-        if (isFocused)
-            getMachines();
+    useEffect(() =>
+    {
+        if (isFocused) getMachines();
     }, [isFocused]);
 
-    function getMachines(): void {
+    function getMachines(): void
+    {
         fetchGetMachine()
-            .then((res: Response) => {
-                if (res.ok) {
+            .then((res: Response) =>
+            {
+                if (res.ok)
+                {
                     return res.json();
                 }
             })
-            .then((res_data) => {
+            .then((res_data) =>
+            {
                 console.log(`GETMACHINES RES_DATA=${res_data}`);
                 setMachine(res_data);
             })
             .catch((error: unknown) => console.log(error));
     }
 
-    function addOneMachine(machine: Machine): void {
+    function addOneMachine(machine: Machine): void
+    {
         console.log(`${machine.name} ${machine.muscle}`);
         fetchPostMachine(machine)
-            .then((res) => {
-                if (res.status === 201) {
+            .then((res) =>
+            {
+                if (res.status === 201)
+                {
                     return res.json();
                 }
             })
-            .then((res_data) => {
+            .then((res_data) =>
+            {
                 console.log(`RES_DATA=${JSON.stringify(res_data)}`);
                 setMachine([...machines, res_data]);
             })
-            .catch((error: unknown) => {
+            .catch((error: unknown) =>
+            {
                 console.log(error);
             });
     }
 
-    function removeOneMachine(name: string) {
+    function removeOneMachine(name: string)
+    {
         fetchDeleteMachine(name)
-            .then((res) => {
-                if (res.ok) {
+            .then((res) =>
+            {
+                if (res.ok)
+                {
                     setMachine(
                         machines.filter((machine) => machine.name !== name),
                     );
                 }
             })
-            .catch((error: unknown) => {
+            .catch((error: unknown) =>
+            {
                 console.log(error);
             });
     }
@@ -84,23 +99,26 @@ function MachinePage() {
     //     />
     // ));
     //
-    function filterMachines() {
-        if (search === "") {
+    function filterMachines()
+    {
+        if (search === "")
+        {
             return machines;
         }
-        return machines.filter((machine: Machine) => {
+        return machines.filter((machine: Machine) =>
+        {
             return machine.name.toLowerCase().includes(search.toLowerCase());
         });
     }
 
     return (
         <SafeAreaView edges={["top"]} className="flex-1 bg-white pt-4">
-            <View className="flex-row justify-between">
-                <Text className="text-3xl font-semibold text-black tracking-tight px-4 pt-4 pb-1">
+            <View className="flex-row justify-between items-center px-4 pt-4 pb-2">
+                <Text className="text-3xl font-semibold text-black tracking-tight">
                     Machines
                 </Text>
                 <Pressable
-                    className="pr-4"
+                    className=""
                     onPress={() => router.push("/newMachine")}
                 >
                     <AntDesign name="plus" size={32} color="black" />
@@ -118,7 +136,7 @@ function MachinePage() {
             />
             <FlatList
                 data={filterMachines()}
-                renderItem={({ item, index }) => (
+                renderItem={({item, index}) => (
                     <MachineComponent
                         key={index}
                         machine={item}
