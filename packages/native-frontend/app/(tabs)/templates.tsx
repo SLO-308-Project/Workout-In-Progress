465a073d9cd64335d087434afa446f4f8a093c70
Paul Motter <pmotter@calpoly.edu>
2025-05-19 15:23:30 -0700

    Feat: templates in-progress.

--- packages/native-frontend/app/(tabs)/templates.tsx
+++ packages/native-frontend/app/(tabs)/templates.tsx
@@ -1,11 +1,39 @@
-import {Text} from "react-native";
+import {AntDesign} from "@expo/vector-icons";
+import {useState} from "react";
+import {Pressable, Text, View, StyleSheet} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
 
-export default function templatesPage()
+export default function TemplatesPage()
 {
+    const [templates, setTemplates] = useState<Template[]>([]);
+
     return (
-        <SafeAreaView>
-            <Text>Test</Text>
+        <SafeAreaView edges={["top"]} className="flex-1 bg-white pt-4">
+            <View className="flex-row justify-between items-center px-4 pt-4 pb-2">
+                <Text className="text-3xl font-semibold text-black tracking-tight">
+                    Templates
+                </Text>
+                <Pressable className="" onPress={() =>
+                {}}>
+                    <AntDesign name="plus" size={32} color="black" />
+                </Pressable>
+            </View>
         </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    containerStyle: {
+        backgroundColor: "#FFF",
+        borderTopWidth: 0,
+        borderBottomWidth: 0,
+    },
+    inputContainerStyle: {
+        backgroundColor: "#F2F2F7",
+        height: 35,
+    },
+    inputStyle: {
+        color: "#000",
+        fontSize: 16,
+    },
+});
