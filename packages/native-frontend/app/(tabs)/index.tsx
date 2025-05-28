a9233e695d3e9be5644192426824312451517228
Paul Motter <pmotter@calpoly.edu>
2025-05-28 01:33:48 -0700

    fix: Simply Reroutes the user.

--- packages/native-frontend/app/(tabs)/index.tsx
+++ packages/native-frontend/app/(tabs)/index.tsx
@@ -1,32 +1,6 @@
-// import { Text, View } from 'react-native';
-import {fetchGetTemplates} from "@/fetchers/templateFetchers";
-import {useTemplateContext} from "@/util/templateContext";
 import {Redirect} from "expo-router";
 
 export default function Index()
 {
-    //Setup all data needed.
-    const {templates, setTemplates} = useTemplateContext();
-    fetchGetTemplates()
-        .then((res) =>
-        {
-            if (res.ok)
-            {
-                res.json()
-                    .then((json) =>
-                    {
-                        setTemplates(json);
-                    })
-                    .catch((err) =>
-                    {
-                        console.log("Parsing Templates Error: ", err);
-                    });
-            }
-        })
-        .catch((err) =>
-        {
-            console.log("Error Retrieving Templates: ", err);
-        });
-
     return <Redirect href="./pastSessions" />;
 }
