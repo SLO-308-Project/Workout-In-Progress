bf1f000ffccd6ca33fbd080e67260d9e23074b6c
Paul Motter <pmotter@calpoly.edu>
2025-05-29 11:07:01 -0700

    fix: random

--- packages/native-frontend/components/sessions/displaySet.tsx
+++ packages/native-frontend/components/sessions/displaySet.tsx
@@ -13,7 +13,7 @@ export default function DisplaySet({machine, set, index}: Props)
     return (
         <View className="bg-gray-300 border border-gray-400 rounded-lg shadow-sm p-2 m-1 justify-center">
             <Text className="border-b border-gray-400 text-xl font-bold">
-                Set{" " + index}
+                Set{" " + (index + 1)}
             </Text>
             <FlatList
                 data={set.attributeValues}
