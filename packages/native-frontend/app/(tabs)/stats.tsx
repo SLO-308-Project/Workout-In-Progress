cb6031524b6f55336b89f8306c9d2bf1966bd67e
David Voronov <davidvoronof@gmail.com>
2025-06-02 08:56:27 -0700

    feat: select machine button

--- packages/native-frontend/app/(tabs)/stats.tsx
+++ packages/native-frontend/app/(tabs)/stats.tsx
@@ -1,16 +1,21 @@
-import {View, Text} from "react-native";
+import {View, Text, Pressable} from "react-native";
 import ProtectedRoute from "@/components/auth/protectedRoute";
 import {SafeAreaView} from "react-native-safe-area-context";
 
 export default function statistics()
 {
     return (
-        <ProtectedRoute>
-            <SafeAreaView>
-                <View>
-                    <Text>This is Statistics</Text>
-                </View>
-            </SafeAreaView>
-        </ProtectedRoute>
+        <SafeAreaView edges={["top"]} className="flex-1 bg-white pt-4">
+            <View className="pl-4 pt-4 pb-2">
+                <Text className="text-3xl font-semibold text-black tracking-tight">
+                    Statistics
+                </Text>
+            </View>
+            <Pressable className="shadow-sm p-4 m-2 rounded-md bg-yellow-400 items-center active:opacity-80 transition-all duration-200">
+                <Text className="color-white font-semibold">
+                    Select Machine
+                </Text>
+            </Pressable>
+        </SafeAreaView>
     );
 }
